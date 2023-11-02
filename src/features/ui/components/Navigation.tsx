e399698f67ab7f65b283ddd8713d12c8eb328950
yoonjiO <ohyescoding@gmail.com>
2023-11-02 21:58:11 +0900

    Feat: Navigation route 연결
    root layer 에서 공통으로 사용되는 Navigation 컴포넌트 안에서 path 를 받아와
    path 별로 보여지는 내용을 분기처리하였습니다.

--- src/features/ui/components/Navigation.tsx
+++ src/features/ui/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import {
   BackButtonIcon,
   CloseButtonIcon,
@@ -9,24 +9,49 @@ import {
 // pathname 을 사용해 각 화면에 맞는 제목과 아이콘 버튼을 표시합니다.
 const Navigation: React.FC = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
-  const onClick = () => {
-    console.log("onClick");
-  };
+
+  // BackButtonIcon (뒤로가기) 보여주는 pathname 을 배열로 만들어서 includes 로 비교
+  const renderBackButtonIcon = ["/"].includes(pathname) as boolean;
+
+  // CloseButtonIcon (뒤로가기) 보여주는 pathname 을 배열로 만들어서 includes 로 비교
+  const renderCloseButtonIcon = ["/register"].includes(pathname) as boolean;
+
+  const title = getTitle(pathname);
+
   return (
     <nav className="fixed top-0 left-0 h-88 w-full bg-white">
       <div className="flex h-full w-full justify-between items-center px-16">
-        <BackButtonIcon onClick={() => navigate(-1)} />
-        <span
+        {renderBackButtonIcon && (
+            <BackButtonIcon onClick={() => navigate(-1)} />
+          )}        <span
           className={
-            "text-center align-text-top text-ebony text-17 font-semibold"
+            "w-full text-center align-text-top text-ebony text-17 font-semibold"
           }>
-          {window.document.title}
+          {title}
         </span>
-        <CloseButtonIcon onClick={onClick} />
+        {renderCloseButtonIcon && (
+          <CloseButtonIcon onClick={() => navigate(-1)} />
+        )}
       </div>
     </nav>
   );
 };
 
 export default Navigation;
+
+
+const getTitle = (pathname: string) => {
+  if (pathname.includes("detail")) {
+    return "상세";
+  } else if (pathname.includes("edit")) {
+    return `${window.document.title} 수정`;
+  } else if (pathname.includes("register")) {
+    return `${window.document.title} 등록`;
+  } else if (pathname === "/") {
+    return window.document.title;
+  } else {
+    return window.document.title;
+  }
+};
