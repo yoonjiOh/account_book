dd087b949efff3c59b169ae493f5d82130742024
yoonjiO <ohyescoding@gmail.com>
2023-11-02 21:16:11 +0900

    Add eslint & prettier 설치
    코드 퀄리티 및 스타일 유지를 위해 설치하였습니다.

---
+++ .eslintrc.cjs
@@ -0,0 +1,72 @@
+module.exports = {
+  root: true,
+  env: {browser: true, es2020: true},
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:storybook/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: {ecmaVersion: "latest", sourceType: "module"},
+  settings: {
+    react: {version: "18.2"},
+    "import/resolver": {
+      alias: {
+        map: [["@", "./src"]],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  plugins: ["react-refresh", "import"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      {allowConstantExport: true},
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "type",
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "unknown",
+        ],
+        pathGroups: [
+          {
+            pattern: "react*",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@hooks/*",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@pages/*",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@components/*",
+            group: "internal",
+            position: "after",
+          },
+        ],
+
+        pathGroupsExcludedImportTypes: ["@tanstack*"],
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+// eslint-disable-next-line import/default
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    eslintPlugin({
+      exclude: ["/virtual:/", "/node_modules/"],
+    }),
+    react(),
+  ],
 });
