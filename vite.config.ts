92cb308b18098ebc3693800b5871233b5c17e731
yoonjiO <ohyescoding@gmail.com>
2023-11-02 21:19:04 +0900

    Add Storybook
    컴포넌트를 독립적으로 만들고 재사용성을 높이고 컴포넌트 테스트 목적으로 storybook 을 설치하였습니다.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
-// eslint-disable-next-line import/default
-import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    eslintPlugin({
-      exclude: ["/virtual:/", "/node_modules/"],
-    }),
     react(),
   ],
 });
